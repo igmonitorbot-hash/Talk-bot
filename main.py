6e7e79922041e77d4051225a6316ae79bb69cc78
igmonitorbot-hash <igmonitorbot@gmail.com>
2026-08-20 23:38:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
-
+model = genai.GenerativeModel("gemini-3.1-flash-lite")
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
